1fbd556ae5a1364688b7521dd5045e4068fe8bbc
anibha88 <anibha88@gmail.com>
2017-05-03 17:50:24 +0530

    recipe and chef model with test coverage

---
+++ test/models/chef_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+  def setup
+    @chef = Chef.new(chefname: "nithin", email: "nithin@example.com")
+  end
+
+  test "should be valid" do
+    assert @chef.valid?
+  end
+
+  test "chefname should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+
+  test "name should be less than 30 characters" do
+    @chef.chefname = "a" * 31
+    assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "email should not be too long" do
+    @chef.email = "a" * 245 + "@example.com"
+    assert_not @chef.valid? 
+  end
+
+  test "email should accept correct format" do
+    # build an array of email addresses 
+    valid_emails = %w[user@example.com nithin@gmail.com M.first@yahoo.ca john+smith@co.uk.org]
+      valid_emails.each do |valids|
+        @chef.email = valids
+        assert @chef.valid?, "#{valids.inspect} should be valid"
+      end
+  end
+
+  test "should reject invalid email addresses" do
+    # build an array of invalid emails
+    invalid_emails = %w[user@example nithin@gmail,com M.first@yahoo. john@smith+foo.com]
+      invalid_emails.each do |invalids|
+        @chef.email = invalids
+        assert_not @chef.valid?, "#{invalids.inspect} should be invalid"
+      end
+  end
+
+  test "email should be unique and case insensitive" do
+    duplicate_chef = @chef.dup
+    duplicate_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not duplicate_chef.valid?
+  end
+
+  test "email should be in lowercase before hitting db" do
+    mixed_email = "jOhn@ExamPle.com"
+    @chef.email = mixed_email
+    @chef.save
+    assert_equal mixed_email.downcase, @chef.reload.email
+  end
+
+end
