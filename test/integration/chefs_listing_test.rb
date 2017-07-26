f0ff45695dbb1a552e6c79d0140d2062dfd56d6c
anibha88 <anibha88@gmail.com>
2017-07-26 12:16:55 +0530

    Added chefs index page with tests

---
+++ test/integration/chefs_listing_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class ChefsListingTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "Nithin", email: "nithin@example.com",
+                         password: "sri hari", password_confirmation: "sri hari")
+    @chef1 = Chef.create!(chefname: "Nikitha", email: "nikitha@example.com",
+                         password: "sri hari", password_confirmation: "sri hari")
+    @chef2 = Chef.create!(chefname: "Ramesh", email: "ramesh@example.com",
+                         password: "sri hari", password_confirmation: "sri hari")
+    @chef3 = Chef.create!(chefname: "Gayathri", email: "gayathri@example.com",
+                         password: "sri hari", password_confirmation: "sri hari")
+    @chef4 = Chef.create!(chefname: "Vani", email: "vani@example.com",
+                         password: "sri hari", password_confirmation: "sri hari")
+    
+  end
+
+  test "should get recipes index" do 
+    get chefs_path
+    assert_response :success
+  end
+
+  test "should get recipes listing" do 
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_select "a[href=?]", chef_path(@chef), text: @chef.chefname.capitalize
+    assert_select "a[href=?]", chef_path(@chef1), text: @chef1.chefname.capitalize
+    assert_select "a[href=?]", chef_path(@chef2), text: @chef2.chefname.capitalize
+    assert_select "a[href=?]", chef_path(@chef3), text: @chef3.chefname.capitalize
+    assert_select "a[href=?]", chef_path(@chef4), text: @chef4.chefname.capitalize
+    assert_select 'a>img.img-circle'
+    assert_match @chef.recipes.count.to_s, response.body
+  end
+end
