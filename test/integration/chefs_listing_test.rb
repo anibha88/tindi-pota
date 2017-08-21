bbf17818cc05b15763b958b28478321d632ba5a6
anibha88 <anibha88@gmail.com>
2017-08-21 13:18:54 +0530

    Delete chefs feature including feature tests

--- test/integration/chefs_listing_test.rb
+++ test/integration/chefs_listing_test.rb
@@ -31,4 +31,14 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
     assert_select 'a>img.img-circle'
     assert_match @chef.recipes.count.to_s, response.body
   end
+
+  test "should delete chef" do
+    get chefs_path 
+    assert_template 'chefs/index'
+    assert_difference 'Chef.count', -1 do
+      delete chef_path(@chef2)
+    end
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+  end
 end
